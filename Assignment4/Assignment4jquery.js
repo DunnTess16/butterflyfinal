37b56cf13c85b7f7758a5b02e3f2780c2c62648d
Tess <literature14@gmail.com>
2018-12-10 15:17:58 -0500

    final

---
+++ Assignment4/Assignment4jquery.js
@@ -0,0 +1,38 @@
+// $(".butterfly1").click(function(){
+// 	$(".butterfly1").hide();
+// })
+
+// $(".butterfly2").click(function(){
+// 	$(".butterfly2").hide();
+// })
+
+// $(".butterfly3").click(function(){
+// 	$(".butterfly3").hide();
+// })
+
+// $(".butterfly4").click(function(){
+// 	$(".butterfly4").hide();
+// })
+
+
+// $(".butterfly5").click(function(){
+// 	$(".butterfly5").hide();
+// })
+
+// $(".butterfly6").click(function(){
+// 	$(".butterfly6").hide();
+// })
+
+$(".container").hover(function(){
+	$(".overlay").css("opacity", "1");
+}
+
+
+
+
+
+$("h5").click(function(){
+	$(window).scrollTo("butterfly19",2000);
+
+})
+
